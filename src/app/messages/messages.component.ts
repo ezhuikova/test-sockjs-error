efe2b21e309146c177aab68209b2cecbaddd3ea4
Ekaterina Zhuikova\kate <ezhuikova@otalio.com>
2018-11-21 11:27:41 +0300

    refactoring

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -26,17 +26,19 @@ export class MessagesComponent implements OnInit, OnDestroy {
     if (this.topicSubscription1 != null) {
       this.topicSubscription1.unsubscribe();
     }
-    this.topicSubscription1 = this.rxStompService.watch('/topic/reservations/diningDateTime/' + date).subscribe((message: Message) => {
-      this.receivedMessages.push(message.body);
-    });
+    this.topicSubscription1 = this.rxStompService.watch('/topic/reservations/diningDateTime/' + date)
+      .subscribe((message: Message) => {
+        this.receivedMessages.push(message.body);
+      });
 
     setTimeout(() => {
       if (this.topicSubscription2 != null) {
         this.topicSubscription2.unsubscribe();
       }
-      this.topicSubscription2 = this.rxStompService.watch('/topic/diningSpecialRequests/requestDate/' + date).subscribe((message: Message) => {
-        this.receivedMessages.push(message.body);
-      });
+      this.topicSubscription2 = this.rxStompService.watch('/topic/diningSpecialRequests/requestDate/' + date)
+        .subscribe((message: Message) => {
+          this.receivedMessages.push(message.body);
+        });
 
     }, 1500);
 
@@ -45,16 +47,12 @@ export class MessagesComponent implements OnInit, OnDestroy {
   ngOnDestroy() {
   }
 
-  onSendMessage() {
-    this.runSubscription();
-  }
 
   private getDate(): string {
     const dateItems = ['20181121-20181122', '20181122-20181123'];
-    if (this.dateIndex == 1) {
+    if (this.dateIndex === 1) {
       this.dateIndex = 0;
-    }
-    else {
+    } else {
       this.dateIndex = +1;
     }
 
